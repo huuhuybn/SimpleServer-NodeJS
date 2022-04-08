572b9914bbf51803f3cbf3bb289affc146385185
HuyNguyen <huuhuybn@gmail.com>
2022-04-08 10:54:27 +0700

    abc

--- routes/index.js
+++ routes/index.js
@@ -197,21 +197,7 @@ router.get('/allMobile', function (req, res) {
 
     // lấy danh sách students
     Student.find({}, function (err, data) {
-        // trả về 1 file ejs.
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:2000');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        res.send((data))
+        res.send(data)
     })
 
 })
